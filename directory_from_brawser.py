99965154ec970fbae1e89f4746094ef440776c36
Aaya Elsharief <aaya.elsh@gmail.com>
2021-06-05 21:32:21 +0300

    server1

---
+++ directory_from_brawser.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+
+PORT = 9000
+
+handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), handler) as httpd:
+    print("Server started at localhost:" + str(PORT))
+    httpd.serve_forever()
+
+
+
+
+
+
+
